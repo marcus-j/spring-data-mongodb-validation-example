36c2a9080db20d3d06ee90855a1975026161f6d5
marcus-j <18111840+marcus-j@users.noreply.github.com>
2021-03-05 20:41:13 +0100

    inits bare Spring repo

---
+++ src/main/java/de/marcusjanke/examples/mongodbvalidation/SpringDataMongodbValidationApplication.java
@@ -0,0 +1,13 @@
+package de.marcusjanke.examples.mongodbvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataMongodbValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataMongodbValidationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/marcusjanke/examples/mongodbvalidation/SpringDataMongodbValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package de.marcusjanke.examples.mongodbvalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataMongodbValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
